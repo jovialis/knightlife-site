5554df1cbe1f18a1a03565dcc99193cb46e0323e
Dylan Hanson <cdh.create@gmail.com>
2019-05-03 20:37:30 -0400

    Insttantiate new Cookie jar

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -3,14 +3,16 @@ import React from 'react';
 import Router from 'next/router';
 
 import axios from 'axios';
-import cookies from 'cookies';
+import Cookies from 'cookies';
 
 import {default as DashboardLayout, HeaderProfileContent, HeaderTitleContent} from '../layouts/dashboard';
 
 export default class extends React.Component {
 
-	static async getInitialProps({req}) {
-		const token = req.cookies.get('Session', {signed: true});
+	static async getInitialProps({req, res}) {
+		let cookies = new Cookies(req, res, [process.env.COOKIE_SECRET]);
+
+		const token = cookies.get('Session', {signed: true});
 		const {data} = await axios.get(`https://api.bbnknightlife.com/user/about?token=${ token }`);
 
 		return {
